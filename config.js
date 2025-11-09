6cd8b7a0681c851eeb7a4ca60225920334c3372b
Sarthak Pande <sarthakpande1008@gmail.com>
2025-11-10 01:18:18 +0530

    updated config file

--- config.js
+++ config.js
@@ -1,9 +1,13 @@
 // config.js
+import dotenv from "dotenv";
+dotenv.config();
+
 export const config = {
-    apiKey: "iCszW7Gc",        // From Angel SmartAPI portal
-    clientCode: "R203115", // e.g., DUMMY1234
-    mpin: "1234",     // Your Angel One login password
-    totpSecret: "HCGH63DLZIMKQU3DBYJVHZH6OU",
-    secretKey: "e6201f11-df9b-4f7b-ac4f-1fdd5da3c87d"
-  };
+  apiKey: process.env.API_KEY,
+  clientCode: process.env.CLIENT_CODE,
+  mpin: process.env.MPIN,
+  totpSecret: process.env.TOTP_SECRET,
+  secretKey: process.env.SECRET_KEY
+};
+
   
